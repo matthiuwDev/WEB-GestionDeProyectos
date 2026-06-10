c82505f03d38c82c0c0f1147156a5790d0475246
Sebastián Mateo Espinosa <sebastian.espinosa@newinntech.com>
2026-06-09 19:30:20 -0500

    Fix: Redirección al Login cuando vence el token.

--- src/app/features/auth/services/auth.service.ts
+++ src/app/features/auth/services/auth.service.ts
@@ -1,23 +1,22 @@
-import { Injectable, inject, signal } from '@angular/core'; // 🛡️ Importar signal
+import { Injectable, inject, signal } from '@angular/core'; 
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router'; // 🛡️ Importar Router
 import { environment } from '../../../../environments/environment.development';
-import { Observable, tap } from 'rxjs'; // 🛡️ tap es para interceptar la respuesta sin modificarla
+import { Observable, tap } from 'rxjs'; 
 import { AuthResponse, LoginCredentials, User } from '../models/auth.interface';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
   private http = inject(HttpClient);
+  private router = inject(Router);
   private apiUrl = `${environment.apiUrl}/auth`;
 
-  // Signal para almacenar el usuario actual. Es como una "caja" reactiva que puede contener un User o null.
   currentUser = signal<User | null>(null);
 
   login(credentials: LoginCredentials): Observable<AuthResponse> {
     return this.http.post<AuthResponse>(`${this.apiUrl}/login`, credentials).pipe(
       tap((response: AuthResponse) => {
         this.saveToken(response.token);
-        
-        // Metemos los datos del usuario en la "caja" reactiva
         this.currentUser.set(response.data); 
       })
     );
@@ -30,5 +29,10 @@ export class AuthService {
   logout(): void {
     localStorage.removeItem('token');
     this.currentUser.set(null);
+    this.router.navigate(['/login']);
+  }
+
+  isLoggedIn(): boolean {
+    return !!localStorage.getItem('token');
   }
 }
